8fcedffe193acaaa5e5cee658e6363db1c17d936
Andressabrand <andbrandsym@gmail.com>
2023-11-17 13:45:25 -0300

    my commit

--- product.js
+++ product.js
@@ -5,5 +5,18 @@ const menuOptions = [
     { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
     { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
     { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
-    ]
-    
+    { name: 'Bacon Burguer', price: 25, vegan: false, src: '././img/-bacon-burger.jpg' },
+    { name: 'Hamburguer Artesanal', price: 38, vegan: false, src: './img/-hamburguer-fresco-com-carne-e-queijo.avif' },
+    { name: 'Hamburguer de Carne Loka', price: 40, vegan: false, src: './img/hamburguer-carne-loka.avif' },
+    { name: 'Hamburguer de Carne Suina', price: 48, vegan: false, src: './img/hamburguer de carne suina.jpg' },
+    { name: 'Combo Hamburguer mais batata', price: 58, vegan: false, src: './img/combo Hamburgur mais batata.jpg' },
+    { name: 'Hamburguer de Lentilha', price: 50, vegan: true, src: './img/Hamburguer de lentilha.webp' },
+    { name: 'Hamburguer de Beterraba', price: 61, vegan: true, src: './img/Hamburguer de beterraba.jpeg' },
+    { name: 'Hamburguer de Legumes ', price: 64, vegan: true, src: './img/hamburguer de legumes e grão de bico.jpeg' },
+    { name: 'Hamburguer de Chia ', price: 68, vegan: true, src: './img/hamburguer de chia.jpg' },
+]
+
+
+
+
+
